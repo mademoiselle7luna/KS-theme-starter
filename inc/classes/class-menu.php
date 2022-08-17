597b7896d723eb52fddb7172def85bdad195336b
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-08-17 21:32:56 +0200

    Add pages and main menu, fix linter errors.

--- inc/classes/class-menu.php
+++ inc/classes/class-menu.php
@@ -6,6 +6,7 @@
 class Menu {
 	public function __construct() {
 		add_action( 'init', [ $this, 'init_menus' ] );
+		add_action( 'init', [ $this, 'register_my_menu' ] );
 	}
 
 	/**
@@ -14,5 +15,9 @@ class Menu {
 	function init_menus(): void {
 		register_nav_menu( 'primary-menu', __( 'Main menu', 'dp' ) );
 		register_nav_menu( 'footer-menu', __( 'Footer menu', 'dp' ) );
+	}
+
+	function register_my_menu() {
+		register_nav_menu( 'main-menu', __( 'Main Menu', 'dp' ) );
 	}
 }
