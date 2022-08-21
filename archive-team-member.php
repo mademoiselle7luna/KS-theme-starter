d086c6de7daec9f1ed5e84596bc7bc7500e67cbb
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-08-21 19:46:52 +0200

    Add custom post type 'Team members', custom taxonomy 'department' and team members page. Fix linter problems

--- archive-team-member.php
+++ archive-team-member.php
@@ -1,28 +1,10 @@
 <?php
 get_header();
 ?>
-
-
 <?php
 $posts_args = [];
 while ( have_posts() ) {
-	if ( has_post_thumbnail() ) :
-		{
-			echo '<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">';
-			the_post_thumbnail();
-			echo '<h2>';
-			the_title();
-			echo '</h2>';
-			echo '</p></a>';
-			the_post();
-			the_content();
-			the_excerpt();
-			the_time();
-			echo '<p>';
-			the_date();
-			echo '</p>';
-		}
-	endif;
+	the_post();
 
 	$thumbnail_url = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' )
 		? get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' )
@@ -38,29 +20,9 @@ while ( have_posts() ) {
 			'permalink' => get_permalink(),
 		]
 	);
-
-
 }
 echo esc_html( get_template_part( 'template-parts/archive', 'content-rows', $posts_args ) );
 echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
 ?>
-
 <?php
-wp_reset_postdata();
-?>
-	<div>
-		<p>
-			<?php
-			the_posts_pagination();
-			?>
-		</p>
-	</div>
-	<a href="<?php the_permalink(); ?>"><?php the_title( '<h2>', '</h2>' ); ?> </a>
-<?php if ( has_post_thumbnail() ) : ?>
-	<a href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail( 'medium' ); ?>
-	</a>
-<?php endif; ?>
-
-<?
 get_footer();
--- functions.php
+++ functions.php
@@ -3,3 +3,35 @@ require_once 'inc/classes/class-theme.php';
 require_once 'inc/helpers/svg.php';
 new Theme();
 
+function team_members() {
+	$args = [
+		'labels'       => [
+			'name'          => 'Team members',
+			'singular_name' => 'Team member',
+		],
+		'hierarchical' => true,
+		'public'       => true,
+		'has_archive'  => true,
+		'supports'     => [ 'title', 'editor', 'thumbnail' ],
+	];
+
+	register_post_type( 'team-member', $args );
+}
+
+add_action( 'init', 'team_members' );
+
+function department_taxonomy() {
+	$args = [
+		'labels'       => [
+			'name'          => 'Department',
+			'singular_name' => 'department',
+		],
+
+		'public'       => true,
+		'hierarchical' => true,
+
+	];
+
+	register_taxonomy( 'department', [ 'team-member' ], $args );
+}
+add_action( 'init', 'department_taxonomy' );
